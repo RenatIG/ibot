03124e9778fea4cdb79f19bbf211411af532291f
RenatIG <36381363+RenatIG@users.noreply.github.com>
2018-04-11 12:22:04 +0300

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 module.exports = {
-    TOKEN: '352860373:AAGAlOYGV0psmB0lGa3eZqC93ShQEaRDbNs',
+    TOKEN: '548475588:AAHLcPJqMxcZ1rA0j6qL_h34P2CUOa9q9kE',
     DB_URL: 'mongodb://localhost:27017/mybot'
 }
